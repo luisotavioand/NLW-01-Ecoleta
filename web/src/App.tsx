d2b39a6f6e72808170d9c8064e1aa92e09e51f3c
Luis Otavio Bernardo de Andrade <luis291099@gmail.com>
2020-06-07 18:14:43 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+
+//JSX: sintaxe de XML dentro do Javascript
+//estado: informações mantidas pelo componente
+function App() {
+
+  return (
+   <Routes/>
+  );
+}
+
+export default App;
